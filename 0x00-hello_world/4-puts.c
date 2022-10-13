6e35ded9808bc9fb396a37e84e630541bdfcf77a
amegavi08 <amegavifrancis@gmail.com>
2022-10-13 11:03:38 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
-A programme to print a given sentence 
-The programme should end with a value of 0
+*main - programme to print a given sentence 
+*return - The programme should end with a value of 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
