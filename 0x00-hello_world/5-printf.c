b9a2655062412eb034c6ce32b6236684814d0f69
amegavi08 <amegavifrancis@gmail.com>
2022-10-13 11:35:38 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*
+*main - Entry point of the programme
+*
+*
+*return : - the value of 0
+*
+*
+*
+*/
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+
+}
