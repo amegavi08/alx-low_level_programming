d65f09db37cbbe8c3cecd0829980e5cf626958c3
amegavi08 <amegavifrancis@gmail.com>
2022-10-18 20:14:59 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*
+* print_alphabet - Prints the alphabet in lower case
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+
+	int a,b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b = <122; b++)
+		{
+			_putchar(b);
+		}
+
+	_putchar('\n');
+
+	}
+
+}
