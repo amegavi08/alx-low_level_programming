ace65d67fae5accfb5292de8dc23daca4ea6b22d
amegavi08 <amegavifrancis@gmail.com>
2022-10-20 03:45:57 -0700

    foundation files created for debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*
+* main - tests functions that prints if integer is positive or negative
+* return: 0
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
+
