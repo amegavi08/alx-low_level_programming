cc5eec804ce47378ded90a286dfa66503d4b6f06
amegavi08 <amegavifrancis@gmail.com>
2022-10-24 17:46:04 -0700

    task 0 - reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes a pointer to a parameter and updates its values.
+* @n: input inteeger.
+* Return: no return.
+*
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
