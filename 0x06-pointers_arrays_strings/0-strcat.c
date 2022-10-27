ab2aa7280e0e51e5af1bac10a50c7527b3a73756
amegavi08 <amegavifrancis@gmail.com>
2022-10-27 04:01:32 -0700

    task 0 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,10 +6,11 @@
 * @src: source.
 * Return: the pointer to dest.
 */
-
 char *_strcat(char *dest, char *src)
 {
-	int count1 = 0, int count2 = 0;
+	int count1 = 0;
+	
+	int count2 = 0;
 
 	while (*(dest + count1) != '\0')
 	{
