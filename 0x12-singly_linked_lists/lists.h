3624236108937437fc2c2ea0a75509b68b5c6425
amegavi08 <amegavifrancis@gmail.com>
2022-11-25 01:27:17 -0800

    0-print_list.c 1-list_len updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,3 +26,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
